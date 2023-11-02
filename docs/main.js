409f76ad5bc41c5c83d030ee335c1c4e202c3d86
shogit <sho.ohta.sho.ohta@gmail.com>
2023-11-02 18:29:58 +0900

    Update main.js
    Changed url to one of my GitHub url.

--- docs/main.js
+++ docs/main.js
@@ -1,7 +1,7 @@
 let url = new URL(window.location.href);
 let params = url.searchParams;
 if (params.get('to') ===  null){
-    window.location.href="https://github.com/QsZyN/redirect";
+    window.location.href="https://github.com/shogit/redirect";
 }else{
     window.location.href=decodeURIComponent(params.get('to'));
 }
