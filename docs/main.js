0878aec267a03b15713994012a9d357eb759091f
shogit <sho.ohta.sho.ohta@gmail.com>
2026-07-19 06:42:00 +0900

    Refactor URL redirection logic with hash support

--- docs/main.js
+++ docs/main.js
@@ -1,7 +1,12 @@
-let url = new URL(window.location.href);
-let params = url.searchParams;
-if (params.get('to') ===  null){
-    window.location.href="https://github.com/shogit/redirect";
-}else{
-    window.location.href=decodeURIComponent(params.get('to'));
+let hash = window.location.hash;
+if (hash && hash.length > 1) {
+    window.location.href = decodeURIComponent(hash.slice(1));
+} else {
+    let url = new URL(window.location.href);
+    let params = url.searchParams;
+    if (params.get('to') === null) {
+        window.location.href = "https://github.com/shogit/redirect";
+    } else {
+        window.location.href = decodeURIComponent(params.get('to'));
+    }
 }
